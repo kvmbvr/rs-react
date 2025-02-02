6f2643a83fd2648b09aa55f26797f32096506a0b
Kambar <kidkambar@gmail.com>
2025-02-02 14:40:56 +0500

    refactor: rewrite Search and App into class components and implement saving in LS

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { Component } from 'react';
 import './App.css';
 import Search from './components/Search';
 
-function App() {
-  return <Search />;
+class App extends Component {
+  render() {
+    return <Search />;
+  }
 }
 
 export default App;
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,12 +1,50 @@
-import React from 'react';
+import { Component, FormEvent } from 'react';
 
-function Search() {
-  return (
-    <div className="search">
-      <input className="input" type="text" />
-      <button className="button">Search</button>
-    </div>
-  );
+type SearchState = {
+  inputValue: string;
+};
+
+class Search extends Component<Record<string, never>, SearchState> {
+  constructor(props: Record<string, never>) {
+    super(props);
+    this.state = { inputValue: '' };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  componentDidMount(): void {
+    try {
+      const inputValue: string | undefined = localStorage
+        .getItem('inputValue')
+        ?.toString();
+      if (inputValue) {
+        this.setState({ inputValue: inputValue });
+      }
+    } catch (error) {
+      console.error('Error reading from localStorage', error);
+    }
+  }
+
+  handleChange(e: FormEvent<HTMLInputElement>): void {
+    this.setState({ inputValue: e.currentTarget.value });
+  }
+
+  handleClick(): void {
+    localStorage.setItem('inputValue', this.state.inputValue);
+  }
+
+  render() {
+    return (
+      <div className="search">
+        <input
+          type="text"
+          value={this.state.inputValue}
+          onChange={this.handleChange}
+          placeholder="Type something"
+        />
+        <button onClick={this.handleClick}>Search</button>
+      </div>
+    );
+  }
 }
 
 export default Search;
